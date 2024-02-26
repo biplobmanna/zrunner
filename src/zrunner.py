7e81d1e5095d3fce26fba942a1371d144bc07a44
biplobmanna <biplobmanna@outlook.com>
2024-02-27 00:23:30 +0530

    update: using argparse for terminal run

--- src/zrunner.py
+++ src/zrunner.py
@@ -1,51 +1,75 @@
 # package imports
-import os
-import psutil
-import sqlite3
+import argparse
 
 # local imports
-import settings
+from db import Sqlite
+from proc import Proc
 
-p = psutil.process_iter(
-    [
-        "name",
-        "status",
-        "cwd",
-        "cmdline",
-    ]
+# setup the arparser
+description = """small lightweight python utility to check for running service,
+and start them back up if they're not running.
+"""
+parser = argparse.ArgumentParser(description=description)
+
+# arguments
+# list all services, their settings, and running status...
+parser.add_argument(
+    "-l",
+    "--list",
+    action=argparse.BooleanOptionalAction,
+    help="list all the services being managed by zrunner, their current settings, and running status",
 )
-zrok_process_list = []
 
-for _p in p:
-    # print(_p.info['name'])
-    # break
-    if "zrok" in _p.info["name"]:
-        zrok_process_list.append(_p.info)
+parser.add_argument(
+    "--migrate",
+    action=argparse.BooleanOptionalAction,
+    help="migrate all the new services added if any",
+)
 
+parser.add_argument(
+    "--services", nargs="*", type=str, help="show all currently active services"
+)
 
-for z in zrok_process_list:
-    print(z)
+parser.add_argument(
+    "--run",
+    nargs="*",
+    help="run named services, or all services if no arguments provided",
+)
 
-
-class Sqlite:
-    def __init__(self):
-        db_path = os.path.join(settings.BASE_DIR, "sqlite.db")
-        self.con = sqlite3.connect(db_path)
-        self.cur = self.con.cursor()
-
-    def create_table_if_dne(self):
-        """iterate through the list of services, and create
-        table if DNE
-        """
-        for s in settings.SERVICES_LIST:
-            _table_name = s.get("table") or s.get("name")
-            # create table if dne
-            _query = f"create table if not exists {_table_name} ({settings.DB_SERVICES_TABLE_SCHEMA})"
-            # query the master table to check if table has been created successfully
-            result = self.cur.execute(f"select name from sqlite_master where name like '{_table_name}'")
-            if len(result.fetchone()) > 0:
-                print(f"select ({_table_name}) created successfully!")
+parser.add_argument(
+    "--stop",
+    nargs="*",
+    help="stop named services, or all services if no arguments provided",
+)
 
 
-s = Sqlite()
-s.create_table_if_dne()
+# parse
+args = parser.parse_args()
+
+# execute by precedence
+if args.list:
+    print("list called")
+    # nothing else is parsed, exit automatically
+    exit(0)
+
+if args.migrate:
+    db = Sqlite()
+    db.migrate()
+    # nothing else is parsed, exit automatically
+    exit(0)
+
+if args.services is not None:
+    print("services called")
+    # nothing else is parsed, exit automatically
+    exit(0)
+
+if args.stop is not None:
+    print("stop called")
+    # nothing else is parsed, exit automatically
+    exit(0)
+
+if args.run is not None:
+    p = Proc()
+    p.run('specialcarrots')
+    # nothing else is parsed, exit automatically
+    exit(0)
