d5f06337defce35e51333d3522e220db68535a0e
biplobmanna <biplobmanna@outlook.com>
2024-02-28 00:38:43 +0530

    update: start, stop, query services
    1. start a service and get pid = pid + 1
    2. get pid after querying `ps` using the `command`
    3. stop the service using pid
    4. stop the service using `os.kill()` and `signla.SIGTERM`

--- src/proc.py
+++ src/proc.py
@@ -1,6 +1,8 @@
 # package imports
 import psutil
 import subprocess
+import os
+import signal
 
 # local imports
 import settings
@@ -27,13 +29,18 @@ class Proc:
         else:
             return False
 
-    def run(self, name: str) -> None:
-        # get meta for the service name
+    def get_services_data(self, name) -> dict[str:str]:
         _data = None
         for s in settings.SERVICES_LIST:
             if s["name"] == name:
                 _data = s
                 break
+        return _data
+
+    def run(self, name: str) -> None:
+        # get meta data for the service name
+        _data = self.get_services_data(name)
+
         # if no match, return
         if not _data:
             print(f"service {name} not found!")
@@ -58,6 +65,51 @@ class Proc:
         # since we're running the command through a shell, the pid returned will be that of the shell
         # so, the pid + 1 (not 100% sure though, needs to be tested thoroughly) should be the command's pid
         _pid = p.pid + 1
+        # find the pid using scanning list of all running services
+        _pid2 = self.find_pid_using_service_command(_data["command"])
+        # if both don't match, then it's a lol situation
+        # this is definite proof that [pid = pid + 1] is non-deterministic
+        if _pid != _pid2:
+            print(f"pid from subprocess don't match that of the actual process")
+            print(f"[pid1={_pid}, pid2={_pid2}]")
+            # use pid2 as this is the correct one
+            _pid = _pid2
 
         # add entry to db
         self.db.save(_table_name, _pid)
+
+    def find_pid_using_service_command(self, service_command):
+        # run `ps ax` to get list of running processes
+        _ps = subprocess.Popen(
+            ["ps", "ax"], stdout=subprocess.PIPE, universal_newlines=True
+        )
+        # iterate through the list of running processes and find the one containing the service_command
+        for line in _ps.stdout:
+            if service_command in line:
+                return line.split(" ")[0]
+
+    def stop(self, name) -> None:
+        _data = self.get_services_data(name)
+
+        # if no match, return
+        if not _data:
+            print(f"service {name} not found!")
+            return
+
+        # irrespective of inactive, still check running task and stop
+        _table_name = _data.get("table") or _data.get("name")
+
+        # 1st pass: check exists, then stop
+        if self.is_running(_table_name):
+            _row = self.db.get_latest(_table_name)
+            _pid = _row[1]
+            _cmd = f"kill -15 {_pid}"
+            p = subprocess.Popen(_cmd, shell=True)
+
+        # 2nd pass: grep the logs path to get the service uniquely
+        _pid = self.find_pid_using_service_command(_data["command"])
+        # if nothing found, no harm, no foul
+        if _pid is None:
+            return
+        # stop the process with _pid using os and signal
+        os.kill(_pid, signal.SIGTERM)
