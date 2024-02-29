cc696093359dafe4c03d948774409f18df4c1c07
biplobmanna <biplobmanna@outlook.com>
2024-03-01 01:19:51 +0530

    add: all() + logging

--- src/proc.py
+++ src/proc.py
@@ -3,6 +3,7 @@ import psutil
 import subprocess
 import os
 import signal
+import logging
 
 # local imports
 import settings
@@ -20,39 +21,58 @@ OPTIONS = [
 class Proc:
     def __init__(self):
         self.db = Sqlite()
+        # logger
+        self.log = logging.getLogger("proc")
+        self.log.setLevel(logging.INFO)
+        # handler
+        file_handler = logging.FileHandler(
+            f"{settings.BASE_DIR}/logs/zrunner.log", mode="a"
+        )
+        file_handler.setLevel(logging.INFO)
+        # formatter
+        formatter = logging.Formatter(
+            "[%(asctime)s :: %(name)s :: %(levelname)s] :: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        # combine all
+        file_handler.setFormatter(formatter)
+        self.log.addHandler(file_handler)
 
     def is_running(self, table_name: str) -> bool:
+        self.log.info(f"is_running({table_name})")
         # getting the latest pid entry
         _row = self.db.get_latest(table_name)
         # check if any matching process with pid (idx=1 in row) is running
-        if _row and psutil.pid_exists(_row[1]):
+        if _row and _row[1] and psutil.pid_exists(_row[1]):
             return True
         else:
             return False
 
     def run(self, name: str) -> None:
+        self.log.info(f"run({name})")
         # get meta data for the service name
         _data = util.get_services_data_by_name(name)
 
         # if no match, return
         if not _data:
-            print(f"service {name} not found!")
+            self.log.error(f"service {name} not found!")
             return
 
         # if the service is marked inactive
         if _data.get("inactive"):
-            print(f"service {name} is marked inactive...")
+            self.log.warning(f"service {name} is marked inactive...")
             return
 
         # query the db, get the latest pid, and check if that's running
         _table_name = _data.get("table") or _data.get("name")
         if self.is_running(_table_name):
             # since it's running, nothing to do, return
-            print(f"service {name} is already up and running...")
+            self.log.info(f"service {name} is already up and running...")
             return
 
         # if nothing is running, let's run this shit
         _cmd = f"{_data['command']} > {settings.BASE_DIR}/logs/{name} 2>&1 &"
+        self.log.info(f"service {name} is starting...")
         p = subprocess.Popen(_cmd, shell=True)
 
         # since we're running the command through a shell, the pid returned will be that of the shell
@@ -63,15 +83,40 @@ class Proc:
         # if both don't match, then it's a lol situation
         # this is definite proof that [pid = pid + 1] is non-deterministic
         if _pid != _pid2:
-            print(f"pid from subprocess don't match that of the actual process")
-            print(f"[pid1={_pid}, pid2={_pid2}]")
+            self.log.warning(
+                f"pid from subprocess don't match that of the actual process"
+            )
+            self.log.warning(f"[pid1={_pid}, pid2={_pid2}]")
             # use pid2 as this is the correct one
             _pid = _pid2
 
         # add entry to db
         self.db.save(_table_name, _pid)
 
+    def run_all(self, names=[]):
+        self.log.info(f"run_all({names})")
+        _names = [s.get("name") for s in settings.SERVICES_LIST]
+        _unmatched = []
+        _matched = []
+        # match that the names are present in the list of services
+        if names:
+            for n in names:
+                if n in _names:
+                    _matched.append(n)
+                else:
+                    _unmatched.append(n)
+        else:
+            _matched = _names
+        # if no matches (applicable only for named inputs)
+        if not _matched:
+            self.log.error(f"the input names did not match any services:{_unmatched}")
+            return
+
+        for n in _matched:
+            self.run(n)
+
     def find_pid_using_service_command(self, service_command):
+        self.log.info(f"find_pid_using_service_command({service_command})")
         # run `ps ax` to get list of running processes
         _ps = subprocess.Popen(
             ["ps", "ax"], stdout=subprocess.PIPE, universal_newlines=True
@@ -82,10 +127,11 @@ class Proc:
                 return line.split(" ")[0]
 
     def stop(self, name) -> None:
+        self.log.info(f"stop({name})")
         _data = util.get_services_data_by_name(name)
         # if no match, return
         if not _data:
-            print(f"service {name} not found!")
+            self.log.error(f"service {name} not found!")
             return
 
         # irrespective of inactive, still check running task and stop
@@ -102,16 +148,40 @@ class Proc:
         _pid = self.find_pid_using_service_command(_data["command"])
         # if nothing found, no harm, no foul
         if _pid is None:
-            print(f"service '{name}' stopped successfully!")
+            self.log.info(f"service '{name}' stopped successfully!")
             return
         # stop the process with _pid using os and signal
         os.kill(_pid, signal.SIGTERM)
 
         # print stop service status
-        print(f"service '{name}' stopped successfully!")
+        self.log.info(f"service '{name}' stopped successfully!")
+
+    def stop_all(self, names=[]):
+        self.log.info(f"stop_all({names})")
+        _names = [s.get("name") for s in settings.SERVICES_LIST]
+        _unmatched = []
+        _matched = []
+        # match that the names are present in the list of services
+        if names:
+            for n in names:
+                if n in _names:
+                    _matched.append(n)
+                else:
+                    _unmatched.append(n)
+        else:
+            _matched = _names
+
+        # if no matches (applicable only for named inputs)
+        if not _matched:
+            self.log.error(f"the input names did not match any services:{_unmatched}")
+            return
+
+        for n in _matched:
+            self.stop(n)
 
     def list_all_services(self, names=[]):
-        _names = [s.get('name') for s in settings.SERVICES_LIST]
+        self.log.info(f"list_all_services({names})")
+        _names = [s.get("name") for s in settings.SERVICES_LIST]
         _unmatched = []
         _matched = []
         # match that the names are present in the list of services
@@ -127,6 +197,7 @@ class Proc:
         # if no matches (applicable only for named inputs)
         if not _matched:
             print(f"the input names did not match any services:\n{_unmatched}")
+            self.log.error(f"the input names did not match any services:{_unmatched}")
             return
 
         print("\n============================================")
@@ -142,7 +213,9 @@ class Proc:
             _latest = self.db.get_latest(_table_name)
             _pid = _latest[1]
             _cmd_list = ["ps", "-Flww", "-p", str(_pid)]
-            _ps = subprocess.Popen(_cmd_list, stdout=subprocess.PIPE, universal_newlines=True)
+            _ps = subprocess.Popen(
+                _cmd_list, stdout=subprocess.PIPE, universal_newlines=True
+            )
             # print all the details below
             for k, v in _data.items():
                 print(f"{k}: {v}")
